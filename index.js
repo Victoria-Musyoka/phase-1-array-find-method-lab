c5846d059720e9e059b25c58d87c207394c582a4
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-13 20:35:41 +0300

    completing assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    function winner(year){
+        return year.result ==="W"
+    }
+    const winningYear = record.find(winner);
+    if(winningYear){
+        return winningYear.year;
+    }
+}
